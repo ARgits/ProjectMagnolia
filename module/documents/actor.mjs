4bd6b5d735e6b889c815268f773734590e6c80a4
Кудрявцев Александр <kydr95.mail.ru@gmail.com>
2021-06-18 08:13:34 +0000

    1

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -64,7 +64,7 @@
       }
     }
     data.attributes.prof_bonus=Math.floor((7+data.attributes.level)/4);
-    data.attributes.prof_die=data.attributes.prof_bonus*2;
+    data.attributes.prof_die="1d"+data.attributes.prof_bonus*2;
   }
 
   /**
