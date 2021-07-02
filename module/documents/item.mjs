7e3d0e12be7d69fde15d5ae5a85e24997ee62077
Кудрявцев Александр <kydr95.mail.ru@gmail.com>
2021-07-02 07:18:57 +0000

    weapon

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -41,7 +41,7 @@ export class ARd20Item extends Item {
       }
       labels.type = game.i18n.localize(CONFIG.ARd20.WeaponType[data.type])??CONFIG.ARd20.WeaponType[data.type];
       labels.prof = game.i18n.localize(CONFIG.ARd20.prof[data.prof.value])??CONFIG.ARd20.prof[data.prof.value];
-      data.prof.label=labels.prof;
+      data.prof.label=labels.prof.toLowerCase();
     }
   }
   /**
