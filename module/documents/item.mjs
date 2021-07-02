904d6b1d9c093860b6d17aa9b029d3a1a087135a
Кудрявцев Александр <kydr95.mail.ru@gmail.com>
2021-07-02 09:23:01 +0000

    weapon

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -39,18 +39,19 @@ export class ARd20Item extends Item {
           data.property.master[k]=true
         }
       }
-      labels.type = game.i18n.localize(CONFIG.ARd20.WeaponType[data.type])??CONFIG.ARd20.WeaponType[data.type];
+      labels.type = game.i18n.localize(CONFIG.ARd20.WeaponType[data.type.value])??CONFIG.ARd20.WeaponType[data.type.value];
       labels.prof = game.i18n.localize(CONFIG.ARd20.prof[data.prof.value])??CONFIG.ARd20.prof[data.prof.value];
-      data.prof.label=labels.prof.toLowerCase();
+      data.prof.label=labels.prof
+      data.type.label=labels.type
       let atk={};
-      if(data.property[data.prof.label].fin===true){
+      if(data.property[data.prof.label.toLowerCase()].fin===true){
         atk.abil=actorData?.dex?.mod+2
-      }else if(data.property[data.prof.label].hea===true){
+      }else if(data.property[data.prof.label.toLowerCase()].hea===true){
         atk.abil=actorData?.dex?.mod-2
       }else{
         atk.abil=actorData?.dex?.mod
       }
-      data.damage.common.current=data.damage.common[data.prof.label]
+      data.damage.common.current=data.damage.common[data.prof.label.toLowerCase()]
     }
   }
   /**
