8911772c9d7541c88994000c87443f046e70e690
Кудрявцев Александр <kydr95.mail.ru@gmail.com>
2021-07-02 08:01:20 +0000

    weapon

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -44,11 +44,11 @@ export class ARd20Item extends Item {
       data.prof.label=labels.prof.toLowerCase();
       let atk={};
       if(data.property[data.prof.label].fin===true){
-        atk.abil=actorData.abilities.dex.mod+2
+        atk.abil=actorData.dex.mod+2
       }else if(data.property[data.prof.label].hea===true){
-        atk.abil=actorData.abilities.dex.mod-2
+        atk.abil=actorData.dex.mod-2
       }else{
-        atk.abil=actorData.abilities.dex.mod
+        atk.abil=actorData.dex.mod
       }
       data.damage.common.current=data.damage.common[data.prof.label]
     }
