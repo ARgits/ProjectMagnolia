2a164fc90f300cd1e742e7f4569464f8635b7d0f
ARgits <kydr95.mail.ru@gmail.com>
2021-07-07 17:52:33 +0300

    weapon

--- module/helpers/socket.js
+++ module/helpers/socket.js
@@ -1,9 +1,7 @@
 export default class ARd20SocketHandler {
     static async updateActorData ( data ) {
         if ( game.user.isGM ) {
-            const actor = data.actor
-            const obj = data.obj
-            await actor.update( obj )
+            await data.actor.update( data.obj )
         }
     }
 }
