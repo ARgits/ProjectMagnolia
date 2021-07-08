7192553bc15b557688e0960bf77a07e612f73af4
ARgits <kydr95.mail.ru@gmail.com>
2021-07-08 10:30:36 +0300

    weapon

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -5,59 +5,60 @@
 export class ARd20ItemSheet extends ItemSheet {
 
     /** @override */
-    static get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
-            classes: ["ard20", "sheet", "item"],
+    static get defaultOptions () {
+        return mergeObject( super.defaultOptions, {
+            classes: [ "ard20", "sheet", "item" ],
             width: 520,
             height: 480,
-            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
-        });
+            tabs: [ { navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" },
+            { navSelector: ".tabs", contentSelector: ".data-section", initial: "untrained" } ]
+        } )
     }
 
     /** @override */
-    get template() {
-        const path = "systems/ard20/templates/item";
+    get template () {
+        const path = "systems/ard20/templates/item"
         // Return a single sheet for all item types.
         // return `${path}/item-sheet.html`;
 
         // Alternatively, you could use the following return statement to do a
         // unique item sheet by type, like `weapon-sheet.html`.
-        return `${path}/item-${this.item.data.type}-sheet.html`;
+        return `${ path }/item-${ this.item.data.type }-sheet.html`
     }
 
     /* -------------------------------------------- */
 
     /** @override */
-    getData() {
+    getData () {
         // Retrieve base data structure.
-        const context = super.getData();
+        const context = super.getData()
 
         // Use a safe clone of the item data for further operations.
-        const itemData = context.item.data;
-        context.labels = this.item.labels;
-        context.config = CONFIG.ARd20;
+        const itemData = context.item.data
+        context.labels = this.item.labels
+        context.config = CONFIG.ARd20
         // Retrieve the roll data for TinyMCE editors.
-        context.rollData = {};
-        const props = [];
-        let actor = this.object?.parent ?? null;
-        if (actor) {
-            context.rollData = actor.getRollData();
+        context.rollData = {}
+        const props = []
+        let actor = this.object?.parent ?? null
+        if ( actor ) {
+            context.rollData = actor.getRollData()
         }
         // Add the actor's data to context.data for easier access, as well as flags.
-        context.data = itemData.data;
-        context.flags = itemData.flags;
-        context.item.isGM = game.user.isGM;
+        context.data = itemData.data
+        context.flags = itemData.flags
+        context.item.isGM = game.user.isGM
         return context
     }
 
     /* -------------------------------------------- */
 
     /** @override */
-    activateListeners(html) {
-        super.activateListeners(html);
+    activateListeners ( html ) {
+        super.activateListeners( html )
 
         // Everything below here is only needed if the sheet is editable
-        if (!this.isEditable) return;
+        if ( !this.isEditable ) return
 
         // Roll handlers, click handlers, etc. would go here.
 
