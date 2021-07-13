79702a3f9b669c053b7f082b42e12fe33231007c
ARgits <kydr95.mail.ru@gmail.com>
2021-07-13 15:34:13 +0300

    adv

--- module/documents/cha-adv.mjs
+++ module/documents/cha-adv.mjs
@@ -7,29 +7,45 @@ export class CharacterAdvancement extends FormApplication {
             template: 'systems/ard20/templates/actor/parts/cha-adv.html',
             id: 'cha-adv',
             width: 600,
-            height: "auto",
+            height: 800,
             tabs: [{navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'stats'}]
         })
     }
 
     getData (options) {
-        const data = {}
-        const abilities = data.abilities = {
+        const original = {}
+        const advanced = {}
+        original.abilities = {
         }
         for (let [k, v] of Object.entries(CONFIG.ARd20.abilities)) {
-            abilities[k] = {}
-            abilities[k].value = foundry.utils.getProperty(this.object.data, `data.abilities.${k}.value`)
-            abilities[k].mod = foundry.utils.getProperty(this.object.data, `data.abilities.${k}.mod`)
-            abilities[k].label = game.i18n.localize(CONFIG.ARd20.abilities[k]) ?? k
+            original.abilities[k] = {}
+            original.abilities[k].value = foundry.utils.getProperty(this.object.data, `data.abilities.${k}.value`)
+            original.abilities[k].mod = foundry.utils.getProperty(this.object.data, `data.abilities.${k}.mod`)
+            original.abilities[k].label = game.i18n.localize(CONFIG.ARd20.abilities[k]) ?? k
         }
+        advanced.abilities = original.abilities
         return {
-            abilities: abilities
+            original: original,
+            advanced: advanced
         }
     }
 
     activateListeners (html) {
         super.activateListeners(html)
+ //       html.find('.change').click(this._Change(this))
     }
+    /*   _Change(event){
+       super._Change(event);
+           const button = event.currentTarget;
+           let value;
+           switch(button.dataset.action){
+               
+               case "plus": 
+               app = 
+               break;} 
+           }
+   
+           */
 
     async _updateObject (event, formData) {
         const data = foundry.utils.expandObject(formData)
