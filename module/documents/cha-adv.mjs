f843126f9c1e988a233d010693f6c84e28c052a2
ARgits <kydr95.mail.ru@gmail.com>
2021-07-16 09:57:48 +0300

    adv

--- module/documents/cha-adv.mjs
+++ module/documents/cha-adv.mjs
@@ -33,36 +33,7 @@ export class CharacterAdvancement extends FormApplication {
                 data.advanced.abilities[k].isEq = false
             }
         }
-        return {
-            advanced:{
-            abilitites:{
-                str:{
-                    value:data.advanced.abilities.str.value,
-                    mod:data.advanced.abilities.str.mod
-                },
-                dex:{
-                    value:data.advanced.abilities.dex.value,
-                    mod:data.advanced.abilities.dex.mod
-                },
-                con:{
-                    value:data.advanced.abilities.con.value,
-                    mod:data.advanced.abilities.con.mod
-                },
-                int:{
-                    value:data.advanced.abilities.int.value,
-                    mod:data.advanced.abilities.int.mod
-                },
-                wis:{
-                    value:data.advanced.abilities.wis.value,
-                    mod:data.advanced.abilities.wis.mod
-                },
-                cha:{
-                    value:data.advanced.abilities.cha.value,
-                    mod:data.advanced.abilities.cha.mod
-                }
-            }
-            }
-        }
+        return advanced
     }
 
     activateListeners (html) {
