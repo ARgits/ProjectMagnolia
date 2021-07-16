d1fe40217b8e217b293b8a9ee762302a46df779c
ARgits <kydr95.mail.ru@gmail.com>
2021-07-16 10:11:56 +0300

    adv

--- module/documents/cha-adv.mjs
+++ module/documents/cha-adv.mjs
@@ -61,6 +61,6 @@ export class CharacterAdvancement extends FormApplication {
         const data = actor.data.data
         console.log(data)
         this.render()
-        return actor.update(data, {'data.abilities': updateData})
+        return actor.update(data, {'abilities': updateData})
     }
 }
