9bab2f563de55d030b13f4e81d9de31115c93018
ARgits <kydr95.mail.ru@gmail.com>
2021-07-26 17:58:08 +0300

    adv

--- module/helpers/settings.mjs
+++ module/helpers/settings.mjs
@@ -1,7 +1,36 @@
-export const registerSystemSettings=function(){
-    game.settings.register("ard20", "gearProfManage",{
-        name:"SETTINGS.ProfManage",
-        scope:"world",
-        type:"String"
+export const registerSystemSettings = function () {
+    game.settings.register("ard20", "gearProfManage", {
+        name: "SETTINGS.ProfManage",
+        label: "SETTINGS.ProfManage",
+        scope: "world",
+        type: ProfFormApp,
+        restricted: false,
     })
+}
+class ProfFormApp extends FormApplication {
+    static get defaultOptions () {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ["ard20"],
+            title: 'Character Advancement',
+            template: 'systems/ard20/templates/actor/parts/cha-adv.html',
+            id: 'cha-adv',
+            width: 800,
+            height: 'auto',
+            tabs: [{navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'stats'}],
+        })
+    }
+    getData (options) {
+        if (!this.data) {
+            data = this.data = {}
+            proto = data.proto = {}
+        }
+        return
+    }
+    activateListeners (html) {
+        super.activateListeners(html)
+        html.find('.add').click(this._onAdd.bind(this))
+    }
+    _onAdd (event) {
+        button = event.currentTarget
+    }
 }
