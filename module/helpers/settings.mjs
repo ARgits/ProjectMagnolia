89e364b70f24b6936e777617040e445004816849
ARgits <kydr95.mail.ru@gmail.com>
2021-07-29 15:54:35 +0300

    adv

--- module/helpers/settings.mjs
+++ module/helpers/settings.mjs
@@ -2,7 +2,7 @@ export const registerSystemSettings = function () {
     game.settings.register('ard20', 'profs', {
         scope: "world",
         config: false,
-        default: [],
+        default: {},
         type: Object
     })
     game.settings.registerMenu("ard20", "gearProfManage", {
