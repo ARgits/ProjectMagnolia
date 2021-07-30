ae6d34ef2dc8f5a9e3e8f33854f659d60261e4a8
ARgits <kydr95.mail.ru@gmail.com>
2021-07-30 15:43:12 +0300

    adv

--- module/helpers/settings.mjs
+++ module/helpers/settings.mjs
@@ -35,7 +35,6 @@ class ProfFormApp extends FormApplication {
             this.data = {}
             this.data.config = duplicate(CONFIG.ARd20.WeaponType)
             console.log('Форма ', this)
-            console.log(formData)
             this.data.prof = duplicate(game.settings.get('ard20', 'profs'))
         }
         const templateData = {
