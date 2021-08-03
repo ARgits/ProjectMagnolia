c563dfbc93f8cfc67be39cfcc29751c21c3a8aa3
ARgits <kydr95.mail.ru@gmail.com>
2021-08-03 15:46:06 +0300

    adv

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -44,7 +44,7 @@ export class ARd20Item extends Item {
       }
       data.type.value = data.type.value || "amb"
       data.settings = game.settings.get('ard20', 'profs').filter((prof) => prof.type === data.type.value)
-      if ((!data.proto) || (data.settings.filter((prof) => prof.name === data.proto)[0] === undefined)) {)
+      if ((!data.proto) || (data.settings.filter((prof) => prof.name === data.proto)[0] === undefined)) {
         data.proto = data.settings[0].name
       } else {
         data.proto = data.proto
