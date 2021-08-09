c67ca27cc2da5170a361966ebf20c521e6e2ca12
ARgits <kydr95.mail.ru@gmail.com>
2021-08-09 17:31:00 +0300

    adv

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -64,7 +64,7 @@ export class ARd20Item extends Item {
       data.keys = []
       //define levels
       data.level = data.level || {}
-      data.level.has = data.level.has || false
+      data.level.has = data.level.has!==undefined ? data.level.has:false
       data.level.max = data.level.has ? data.level.max || 4 : 1
       data.level.current = data.level.has ? data.level.current || 1 : 1
       //define exp cost
