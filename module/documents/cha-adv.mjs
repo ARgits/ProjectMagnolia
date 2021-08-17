aed36d77ecb9235fac66138dcad3ad27d6fea038
ARgits <kydr95.mail.ru@gmail.com>
2021-08-17 16:54:28 +0300

    adv

--- module/documents/cha-adv.mjs
+++ module/documents/cha-adv.mjs
@@ -394,10 +394,10 @@ export class CharacterAdvancement extends FormApplication {
         }
         if (feats_data.new.length > 0) {
             await actor.createEmbeddedDocuments("Item", feats_data.new.map(item => (item.ItemData)))
-            const feats = Array.from(actor.data.items).slice(-feats_data.new.length)
-            for (let [k, v] of Object.entries(feats)) {
-                await actor.updateEmbeddedDocuments('Item', feats_data.new.map(item => ({_id: v.id, 'data.level.initial': item.data.level.initial})))
-            }
+            /* const feats = Array.from(actor.data.items).slice(-feats_data.new.length)
+             for (let [k, v] of Object.entries(feats)) {
+                 await actor.updateEmbeddedDocuments('Item', feats_data.new.map(item => ({_id: v.id, 'data.level.initial': item.data.level.initial})))
+             }*/
         }
     }
 }
