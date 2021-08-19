a516934ef75a027399a824428f408e103dcb384b
ARgits <kydr95.mail.ru@gmail.com>
2021-08-19 16:26:58 +0300

    adv

--- module/documents/cha-adv.mjs
+++ module/documents/cha-adv.mjs
@@ -126,6 +126,7 @@ export class CharacterAdvancement extends FormApplication {
             }
           }
         }
+        temp_feat_list[k].data.source.label = game.i18n.localize(temp_feat_list[k].data.source.value)
       }
       temp_feat_list = temp_feat_list.filter(
         (item) =>
@@ -155,6 +156,7 @@ export class CharacterAdvancement extends FormApplication {
           this.data.count.feats.psy += 1;
         }
       }
+      
 
       this.data.hover = {
         value: "",
