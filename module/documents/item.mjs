4d52d6a8c63373c8c99857e56d48f37cc7758db5
ARgits <kydr95.mail.ru@gmail.com>
2021-08-23 10:23:57 +0300

    adv

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -123,22 +123,26 @@ export class ARd20Item extends Item {
     /*get items from Compendiums. In settings 'feat'.packs you input name of needed Compendiums*/
     for (let key of game.settings.get("ard20", "feat").packs) {
       if (game.packs.filter((pack) => pack.metadata.label === key).length !== 0) {
-        pack_list.push(Array.from(game.packs.filter((pack) => pack.metadata.label === key && pack.metadata.entity === "Item")[0].index));
-        pack_list = pack_list.flat();
+        let feat_list = [];
+        feat_list.push(Array.from(game.packs.filter((pack) => pack.metadata.label === key && pack.metadata.entity === "Item")[0].index));
+        feat_list = feat_list.flat();
+        for (let feat of feat_list) {
+          pack_list.push(feat.name);
+        }
       }
     }
     console.log("packs", pack_list);
     /* same as above, but for folders*/
     for (let key of game.settings.get("ard20", "feat").folders) {
       if (game.folders.filter((folder) => folder.data.name === key).length !== 0) {
-        let temp_list = [];
-        temp_list.push(game.folders.filter((folder) => folder.data.name === key && folder.data.type === "Item")[0].content);
-        console.log(temp_list);
-        folder_list = temp_list;
-        folder_list = folder_list.flat();
+        let feat_list = [];
+        feat_list.push(game.folders.filter((folder) => folder.data.name === key && folder.data.type === "Item")[0].content);
+        feat_list = feat_list.flat();
+        for (let feat of feat_list) {
+          folder_list.push(feat.name);
+        }
       }
     }
-
     console.log("folders", folder_list);
   }
   /*
