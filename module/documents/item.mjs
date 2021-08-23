5a96b093dd548541ffc65c0fd2730be87505c087
ARgits <kydr95.mail.ru@gmail.com>
2021-08-23 10:10:58 +0300

    adv

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -131,8 +131,9 @@ export class ARd20Item extends Item {
     /* same as above, but for folders*/
     for (let key of game.settings.get("ard20", "feat").folders) {
       if (game.folders.filter((folder) => folder.data.name === key).length !== 0) {
+        console.log(folder_list);
         folder_list.push(game.folders.filter((folder) => folder.data.name === key && folder.data.type === "Item")[0].content);
-        console.log(folder_list)
+        console.log(folder_list);
         folder_list = folder_list.flat();
       }
     }
