1b55d471b40db02d3d4749be4cb985c6138849fe
ARgits <kydr95.mail.ru@gmail.com>
2021-08-23 17:03:22 +0300

    adv

--- module/helpers/cha-adv.mjs
+++ module/helpers/cha-adv.mjs
@@ -167,7 +167,7 @@ export class CharacterAdvancement extends FormApplication {
       object.isEq = object.data.level.initial === object.data.level.current || object.data.level.initial === 0 ? true : false;
       object.isXP = object.data.level.initial === object.data.level.max || object.data.level.xp > this.data.xp.get ? true : false;
       for (let [key, ability] of Object.entries(object.data.req.abilities)) {
-        ability.pass = ability.value >= this.data.abilities[key].value ? false : true;
+        ability.pass = ability.value > this.data.abilities[key].value ? false : true;
         object.isXP = ability.pass ? object.isXP : true;
       }
       for (let [key, skill] of Object.entries(object.data.req.skills)) {
@@ -176,7 +176,7 @@ export class CharacterAdvancement extends FormApplication {
       }
       for (let [key, feat] of Object.entries(object.data.req.feats)) {
         feat.pass =
-          feat.minLevel >= this.data.feats.awail[key].data.level.initial || feat.minLevel >= this.data.feats.learned[key].data.level.initial
+          feat.minLevel > this.data.feats.awail[key].data.level.initial || feat.minLevel > this.data.feats.learned[key].data.level.initial
             ? false
             : true;
         object.isXP = feat.pass ? object.isXP : true;
