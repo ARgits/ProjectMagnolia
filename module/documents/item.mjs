d05b8241abe8933737bac07c61cd0e0492b677e3
ARgits <kydr95.mail.ru@gmail.com>
2021-11-12 13:14:38 +0300

    111

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -94,9 +94,8 @@ export class ARd20Item extends Item {
     if (itemData.type !== "feature") return;
     const data = itemData.data;
     // Handle Source of the feature
-    data.source.value = data.source.value || ["mar"];
-    labels.source = game.i18n.localize(CONFIG.ARd20.source[data.source.value]);
-    data.source.label = labels.source;
+    //labels.source = game.i18n.localize(CONFIG.ARd20.source[data.source.value]);
+    //data.source.label = labels.source;
     data.keys = [];
     //define levels
     data.level.has = data.level.has !== undefined ? data.level.has : false;
