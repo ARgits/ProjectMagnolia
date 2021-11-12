6292499d8f44c7a41445cd00c8fc804b7f3dabf7
ARgits <kydr95.mail.ru@gmail.com>
2021-11-12 12:33:54 +0300

    correcting values

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -94,7 +94,7 @@ export class ARd20Item extends Item {
     if (itemData.type !== "feature") return;
     const data = itemData.data;
     // Handle Source of the feature
-    data.source.value = data.source.value || "mar";
+    data.source.value = data.source.value || ["mar"];
     labels.source = game.i18n.localize(CONFIG.ARd20.source[data.source.value]);
     data.source.label = labels.source;
     data.keys = [];
