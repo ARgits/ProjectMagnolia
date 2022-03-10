c1af255f5f06466533fd9ebb863682bb5908da38
ARgits <kydr95.mail.ru@gmail.com>
2022-03-10 13:05:02 +0300

    111

--- svelte/advancement-rate/advancement-rate-shell.svelte
+++ svelte/advancement-rate/advancement-rate-shell.svelte
@@ -3,8 +3,6 @@
 <script>
   import { getContext } from "svelte";
   import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
-import { each } from "svelte/internal";
-import { arr_entries } from "../../typescript/ard20";
   const { application } = getContext("external");
   export let form;
   export let advancementSetting;
@@ -15,18 +13,18 @@ import { arr_entries } from "../../typescript/ard20";
   }
   function changeSetting() {
     game.settings.set("ard20", "advancement-rate", advancementSetting);
-    const variables = game.settings.get("ard20","advancement-rate").variables
-    const formulas = game.settings.get("ard20","advancement-rate").formulas
-    attributeFormula = formulas.attributes
-    for (let variable of Object.values(variables)){
-      attributeFormula = attributeFormula.replace(variable.shortName,variable.value)
+    const variables = game.settings.get("ard20", "advancement-rate").variables;
+    const formulas = game.settings.get("ard20", "advancement-rate").formulas;
+    attributeFormula = formulas.attributes;
+    for (let variable of Object.values(variables)) {
+      attributeFormula = attributeFormula.replace(variable.shortName, variable.value);
     }
   }
   function requestSubmit() {
     form.requestSubmit;
     game.settings.set("ard20", "advancement-rate", advancementSetting);
   }
-  let attributeFormula
+  let attributeFormula = advancementSetting.formulas.attributes;
   console.log(application);
   console.log(advancementSetting);
   console.log(form);
@@ -38,15 +36,17 @@ import { arr_entries } from "../../typescript/ard20";
       <div class="flexrow">
         <label>CustomValues</label>
         {#each Object.values(advancementSetting.variables) as variable}
+          {(console.log({ variable }), "")}
           <label>{variable.longName}</label>
-          <input bind:value={variable.shortName} on:change={changeSetting} placeholder="shortName">
-          <input bind:value={variable.value} on:change={changeSetting} placeholder="custom value">
+          <input bind:value={variable.shortName} on:change={changeSetting} placeholder="shortName" />
+          <input bind:value={variable.value} on:change={changeSetting} placeholder="custom value" />
         {/each}
-      <div>
-        <label>Non-custom Values</label>
-        <div>As - Attribute Score</div>
-        <div>SS - Skill Score</div>
-        <div>SL - Skill level</div>
+        <div>
+          <label>Non-custom Values</label>
+          <div>As - Attribute Score</div>
+          <div>SS - Skill Score</div>
+          <div>SL - Skill level</div>
+        </div>
       </div>
     </section>
     <section>
