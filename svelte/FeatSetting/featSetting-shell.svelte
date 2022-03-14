1a843009528ab010d481622dfd7fcc90573e3f6d
ARgits <kydr95.mail.ru@gmail.com>
2022-03-14 09:56:14 +0300

    111

--- svelte/FeatSetting/featSetting-shell.svelte
+++ svelte/FeatSetting/featSetting-shell.svelte
@@ -1,7 +1,22 @@
 <svelte:options accessors={true} />
 
-<script context="module">
-  export async function deleteEntry(type, id) {
+
+<script>
+  import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
+  import { uuidv4 } from "@typhonjs-fvtt/runtime/svelte/util";
+  export let elementRoot;
+  export let featSetting = game.settings.get("ard20", "feat");
+  console.log(featSetting);
+
+  async function addEntry(type) {
+    const name = `New ${type}`.slice(0, -1);
+    const id = uuidv4();
+    featSetting[type] = [...featSetting[type], { name: name, id: id }];
+    console.log(featSetting);
+    await game.settings.set("ard20", "feat", featSetting);
+    featSetting = featSetting;
+  }
+  async function deleteEntry(type, id) {
     console.log(type);
     const index = featSetting[type].findIndex((entry) => entry.id === id);
     if (index >= 0) {
@@ -11,25 +26,9 @@
       featSetting = featSetting;
     }
   }
-  export async function changeSetting() {
+  async function changeSetting() {
     await game.settings.set("ard20", "feat", featSetting);
     console.log(featSetting);
-  }
-</script>
-
-<script>
-  import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
-  import { uuidv4 } from "@typhonjs-fvtt/runtime/svelte/util";
-  import FeatPack from "./FeatPack.svelte";
-  export let elementRoot;
-  let featSetting = game.settings.get("ard20", "feat");
-  async function addEntry(type) {
-    const name = `New ${type}`.slice(0, -1);
-    const id = uuidv4();
-    featSetting[type] = [...featSetting[type], { name: name, id: id }];
-    console.log(featSetting);
-    await game.settings.set("ard20", "feat", featSetting);
-    featSetting = featSetting;
   }
 </script>
 
@@ -38,13 +37,19 @@
     <div class="flexcol">
       packs
       {#each featSetting.packs as pack (pack.id)}
-        <FeatPack {pack} type={"packs"} />
+        <div class="grid grid-2col">
+          <input type="text" bind:value={pack.name} on:change={changeSetting} />
+          <button on:click={() => deleteEntry("packs", pack.id)} class="minus far fa-minus-square" />
+        </div>
       {/each}
       <button on:click={() => addEntry("packs")} class="add far fa-plus-square" />
       <hr />
       folders
       {#each featSetting.folders as folder (folder.id)}
-        <FeatPack {folder} type={"folders"} />
+        <div class="grid grid-2col">
+          <input type="text" bind:value={folder.name} on:change={changeSetting} />
+          <button on:click={() => deleteEntry("folders", folder.id)} class="minus far fa-minus-square" />
+        </div>
       {/each}
       <button on:click={() => addEntry("folders")} class="add far fa-plus-square" />
     </div>
