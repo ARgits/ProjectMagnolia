556c5b375adea5eae6ece548ec047cae2918070b
ARgits <kydr95.mail.ru@gmail.com>
2022-05-03 18:16:38 +0300

    fix ability rolls

--- built/documents/actor.js
+++ built/documents/actor.js
@@ -166,7 +166,6 @@ export class ARd20Actor extends Actor {
     if (options?.parts?.length > 0) {
       parts.push(...options.parts);
     }
-    console.log('rollAttributeTest       label: ',label,' attr: ',attr,' data: ',data)
     // Roll and return
     const rollData = foundry.utils.mergeObject(options, {
       parts: parts,
