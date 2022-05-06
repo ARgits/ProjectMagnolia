9ed2161e6278f4d11dcb5eb17219050e07871ecd
ARgits <kydr95.mail.ru@gmail.com>
2022-05-06 12:52:11 +0300

    fix socket handling

--- built/helpers/socket.js
+++ built/helpers/socket.js
@@ -1,11 +1,11 @@
 export default class ARd20SocketHandler {
   //@ts-expect-error
   static async updateActorData(data) {
-      console.log('Socket Called, its GM:', game.user.isGM,' and its active: ',game.user.isActive)
+      console.log('Socket Called, its GM:', game.user.isGM,' and its active: ',game.user.active)
     if (!game.user.isGM) return;
     // if the logged in user is the active GM with the lowest user id
     const isResponsibleGM = game.users
-      .filter((user) => user.isGM && user.isActive)
+      .filter((user) => user.isGM && user.active)
       .some((other) => other.data._id < game.user.data._id);
 
     if (!isResponsibleGM) return;
