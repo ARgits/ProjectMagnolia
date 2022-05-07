08cbca088e6b05145ace841706892faf727e9166
ARgits <kydr95.mail.ru@gmail.com>
2022-05-07 11:33:09 +0300

    little ui fix

--- built/helpers/Character Advancement/Tabs.svelte
+++ built/helpers/Character Advancement/Tabs.svelte
@@ -6,7 +6,7 @@
   const element = getContext("chaAdvElementParameters")
   let minBoxSize;
   $: {
-    minBoxSize = Object.entries($data[activeTab]).length * $element.trHeight;
+    minBoxSize = Object.entries($data[activeTab]).length * $element.trHeight*1.1;
   }
 </script>
 
