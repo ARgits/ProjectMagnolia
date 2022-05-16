502a392deda2731befe86205b7c8736630487dec
ARgits <kydr95.mail.ru@gmail.com>
2022-05-16 15:25:05 +0300

    first try for ItemSheet

--- built/sheets/svelte/ItemShell.svelte
+++ built/sheets/svelte/ItemShell.svelte
@@ -3,6 +3,7 @@
 <script>
   import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
   export let elementRoot;
+  console.log(elementRoot,this)
 </script>
 
 <ApplicationShell bind:elementRoot>
