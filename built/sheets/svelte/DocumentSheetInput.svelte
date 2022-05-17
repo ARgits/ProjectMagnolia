84c5dc72c0e907bc8756ab3c73d9a4befb3fb42f
ARgits <kydr95.mail.ru@gmail.com>
2022-05-17 10:54:00 +0300

    first try for ItemSheet

---
+++ built/sheets/svelte/DocumentSheetInput.svelte
@@ -0,0 +1,13 @@
+<svelte:options accessors={true} />
+<script>
+    import {getContext} from "svelte"
+    export let document
+    export let value
+    export let path
+    async function updateDocument(){
+        data = {}
+        data[path] = value
+        await document.update(data)
+    }
+</script>
+<input bind:value={value} on:change={updateDocument}/>
--- built/sheets/svelte/ItemShell.svelte
+++ built/sheets/svelte/ItemShell.svelte
@@ -2,18 +2,21 @@
 
 <script>
   import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
+  import Input from "./DocumentSheetInput.svelte"
   import { getContext, setContext, afterUpdate, tick } from "svelte";
   import { writable } from "svelte/store";
   import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store";
   export let elementRoot;
   const { application } = getContext("external");
+  setContext("document",$doc)
+  const item = application.object
   const doc = new TJSDocument(application.object);
   let updateData = "";
   afterUpdate(async () => {
     console.log("afterUpdate");
     if (updateData) {
       console.log(updateData);
-      await $doc.update(updateData);
+      await item.update(updateData);
       await tick();
     } else updateData = { name: $doc.name, data: $doc.data.data };
   });
@@ -22,5 +25,5 @@
 <ApplicationShell bind:elementRoot>
   <div>blank sheet</div>
   <div>Name: {$doc.data.name}</div>
-  <input bind:value={$doc.data.name} />
+  <Input value={$doc.data.name} document={item} path={"name"}/>
 </ApplicationShell>
