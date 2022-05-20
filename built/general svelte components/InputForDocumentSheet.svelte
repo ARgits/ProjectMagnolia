37b57721257ff3bad0a8fdd184125ac6ee85696a
ARgits <kydr95.mail.ru@gmail.com>
2022-05-20 18:37:34 +0300

    moving to v10

--- built/general svelte components/InputForDocumentSheet.svelte
+++ built/general svelte components/InputForDocumentSheet.svelte
@@ -1,12 +1,20 @@
 <svelte:options accessors={true} />
 
 <script>
-    import {getContext} from "svelte"
+  import { getContext } from "svelte";
   export let value;
-  const document = getContext('DocumentSheetObject')
-  console.log(document)
+  const document = getContext("DocumentSheetObject");
+  console.log(document);
   let data;
-  $:data = {img:document.img, system:document.system, flags:document.flags}
+  $: {
+    data = { img: document.img, system: document.system, flags: document.flags };
+    console.log(data);
+  }
 </script>
 
-Name: <input bind:value={value} on:change={()=>{document.update(data)}} />
+<input
+  bind:value
+  on:change={() => {
+    document.update(data);
+  }}
+/>
