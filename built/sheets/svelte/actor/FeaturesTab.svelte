6629fde45159ec5b94b7458ca7756de87fd80944
ARgits <kydr95.mail.ru@gmail.com>
2022-06-16 09:57:56 +0300

    features tab

---
+++ built/sheets/svelte/actor/FeaturesTab.svelte
@@ -0,0 +1,12 @@
+<svelte:options accessors={true} />
+<script>
+  import { getContext } from "svelte";
+  const doc = getContext("DocumentSheetObject");
+  let highlight = "";
+</script>
+<ul>
+{#each $doc.items as item}
+  <li>Name: {item.name}</li>
+{/each}
+</ul>
+<style lang="scss"></style>
